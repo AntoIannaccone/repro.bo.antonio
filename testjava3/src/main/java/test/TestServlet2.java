1eeec9b9bed81f67d02ab18443683b9524bcd025
Antonio <Antonio@LAPTOP-GPM0CA74>
2023-01-11 11:21:04 +0100

    initial commit

---
+++ testjava3/src/main/java/test/TestServlet2.java
@@ -0,0 +1,99 @@
+package test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet2
+ */
+@WebServlet("/TestServlet2")
+public class TestServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet2() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet - START");
+		//ESEGUO CONNESSIONE AL DB
+		String output = "" ;
+		try {
+			output = selectFromDatabase();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//
+		System.out.println("doGet - END: " + output);
+		response.getWriter().append(output);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	private String selectFromDatabase () throws SQLException, ClassNotFoundException {
+		
+		System.out.println("START selectFromDatabase");
+		StringBuilder sb = new StringBuilder () ;
+
+		String driver = "org.mariadb.jdbc.Driver";
+		Class.forName(driver);
+		
+		System.out.println("classe " + driver + " e' nel cp");
+
+		// Connessione JDBC e invio credenziali
+		String url = "jdbc:mariadb://centauri.proximainformatica.com:193/academyfs07";
+		Connection con = DriverManager.getConnection(url, "acfs07", "acfs07");
+
+		System.out.println("sono riuscito a connettermi al database urs: " + url);
+
+		Statement cmd = con.createStatement();
+		String query = "select * from messages;";
+		ResultSet res = cmd.executeQuery(query);
+		
+        System.out.println("DEBUG result set fetch size: " + res.getFetchSize());
+        
+		while (res.next()) {
+			sb.append(res.getString(1));
+			sb.append(" - ");
+			sb.append(res.getString(2));
+			sb.append("\n");
+			
+//			returnValue += res.getString(1) ;
+//			returnValue = returnValue + " - " ;
+		}
+		
+		res.close();
+		cmd.close();
+		
+		System.out.println("doGet - END: " + sb.toString ());
+		return sb.toString () ;
+	}
+
+}
